046743dc55a6adb4a89acdaa00159bd9a730a731
Abdullah <143931418+TypeAbdullah@users.noreply.github.com>
2025-06-30 10:45:54 +0600

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -26,18 +26,6 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 ]
             )
         )
-elif data == "commands":
-        await query.message.edit_text(
-            text=CMD_TXT.format(first=query.from_user.first_name),
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("Close", callback_data='close')
-                    ]
-                ]
-            )
-        )
     elif data == "start":
         await query.message.edit_text(
             text=START_MSG.format(first=query.from_user.first_name),
